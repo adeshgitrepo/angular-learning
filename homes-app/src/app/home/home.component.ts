d1c27de7e350722a47a9087a0b00e2b5eb0efb28
adesh sharma <emailtoaksharma@gmail.com>
2024-12-27 01:55:30 -0500

    Added kendo grid implementation

--- homes-app/src/app/home/home.component.ts
+++ homes-app/src/app/home/home.component.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { HousingLocationComponent } from '../housing-location/housing-location.component';
 import { HousingLocation } from '../HousingLocation';
 import { HousingService } from '../housing.service';
+import { KENDO_GRID } from '@progress/kendo-angular-grid';
+import '@angular/localize/init' 
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, HousingLocationComponent],
+  imports: [CommonModule, HousingLocationComponent, KENDO_GRID],
   template: `
     <section>
       <form>
@@ -15,6 +17,19 @@ import { HousingService } from '../housing.service';
         <button class="primary" type="button" (click)="filterResults(filter.value)">Search</button>
       </form>
     <section>
+    <section class="gridResults">
+      <kendo-grid [data]="filteredLocationList">
+        <kendo-grid-column field="id" title="ID"> </kendo-grid-column>
+        <kendo-grid-column field="name" title="Name"> </kendo-grid-column>        
+        <kendo-grid-column field="city" title="city"> </kendo-grid-column>
+        <kendo-grid-column field="state" title="state"> </kendo-grid-column>
+        <kendo-grid-column title="photo">
+          <ng-template kendoGridCellTemplate let-dataItem>
+            <img [src]="dataItem.photo" alt="Image" class="housingLocationPhoto" />
+          </ng-template>
+        </kendo-grid-column>      
+      </kendo-grid>
+    </section>
     <section class="results">
       <app-housing-location *ngFor="let housingLocation 
       of filteredLocationList" [housingLocation]
@@ -28,7 +43,7 @@ export class HomeComponent {
   housingLocationList: HousingLocation [] = [];
   housingService: HousingService = inject(HousingService);
   filteredLocationList: HousingLocation [] = [];
-
+  
   constructor(){
     this.housingService.getAllHoustingLocations().then((housingLocationList: HousingLocation[]) => {
       this.housingLocationList = housingLocationList;
